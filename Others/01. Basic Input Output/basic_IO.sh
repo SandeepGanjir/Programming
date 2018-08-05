e65c03e4ca827c9408e04c99167b14423d56fb0c
Sandeep Ganjir <sandyganjir@gmail.com>
2018-08-05 17:09:13 +0530

    Basic Input Output Problem

---
+++ Others/01. Basic Input Output/basic_IO.sh
@@ -0,0 +1,23 @@
+# Small Program to Read a user's name and greet them.
+
+function abbreviate {
+    local shortName=''
+    for word in $@
+    do
+        shortName=$(echo "$shortName${word:0:1}. ")
+    done
+    echo -n $shortName
+}
+
+#### Console Input ####
+echo
+echo 'User Input Mode: '
+echo 'Please Enter Your Name: '
+read name
+echo "Good Morning $(abbreviate $name)! How are you today?"
+
+#### File Input ####
+echo
+echo 'File Input Mode: '
+[[ -e input.txt ]] || $(echo "Sandeep Kumar Ganjir" > input.txt)
+echo "Good Evening $(abbreviate $(cat input.txt))! How are you today?"
