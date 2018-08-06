6847d6d2ee708ccb24792392f79ec6ba731dd909
Sandeep Ganjir <sandyganjir@gmail.com>
2018-08-06 23:38:42 +0530

    basic IO Python

---
+++ Others/01. Basic Input Output/basic_IO.py
@@ -0,0 +1,16 @@
+def greetFromUserInput():
+    print("\nUser Input Mode: ")
+    print("Please Enter Your Name: ")
+    print("Good Morning ")
+    # + abbreviateName(name) + 
+    ("! How are you today?")
+
+def greetFromFileInput():
+    print("\nFile Input Mode: ")
+    print("Good Evening ")
+
+def main():
+    greetFromUserInput()
+    greetFromFileInput()
+
+main()
