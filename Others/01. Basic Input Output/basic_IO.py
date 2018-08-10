fd76065b1fe11fdae29dc6013a6fd38cdc740a14
Sandeep Ganjir <sandyganjir@gmail.com>
2018-08-10 23:52:50 +0530

    Python basic IO done

--- Others/01. Basic Input Output/basic_IO.py
+++ Others/01. Basic Input Output/basic_IO.py
@@ -1,13 +1,32 @@
+# Small Program to Read a user's name and greet them with Abbreviated name.
+
+def abbreviateName(p_Name):
+    result = ""
+    flag = True
+    for c in p_Name:
+        if (c == ' '):
+            flag = True
+        else:
+            if flag:
+                result = result + c + ". "
+                flag = False
+    result = result[:-1]
+    return result
+
 def greetFromUserInput():
     print("\nUser Input Mode: ")
-    print("Please Enter Your Name: ")
-    print("Good Morning ")
-    # + abbreviateName(name) + 
-    ("! How are you today?")
+    name = input("Please Enter Your Name: ")
+    print("Good Morning ", abbreviateName(name), "! How are you today?", sep='', end='\n')
 
 def greetFromFileInput():
     print("\nFile Input Mode: ")
-    print("Good Evening ")
+    name = ""
+    try:
+        fp = open('input.txt')
+        name = fp.readline()
+    finally:
+        fp.close()
+    print("Good Evening", abbreviateName(name), "! How are you today?", sep=' ', end='\n')
 
 def main():
     greetFromUserInput()
