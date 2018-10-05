dc73a02ccbb8d235999aad3bdb32a5738bf6bf54
Sandeep Ganjir <sandyganjir@gmail.com>
2018-10-05 23:32:41 +0530

    Added Palindrome

---
+++ Java/Algorithmic/Palindrome.java
@@ -0,0 +1,56 @@
+/*
+ * A small program to test if a given String is a Palindrome and if any permutation is a Palindrome.
+ *  @author Sandeep Ganjir
+ */
+
+import java.util.*;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        test("Girafarig");
+        test("Mississippi");
+        test("Sandeep");
+    }
+
+    public static void test(String p_Str) {
+        String cond = isPermutationPalindrome(p_Str) ? "" : "not ";
+        String str1 = "\n\"" + p_Str + "\" is " + cond + "a Permutation Palindrome";
+        System.out.println(str1);
+
+        cond = isPalindrome(p_Str) ? "" : "not ";
+        String str2 = "\"" + p_Str + "\" is " + cond + "a Palindrome";
+        System.out.println(str2);
+    }
+
+    public static boolean isPermutationPalindrome(String p_Str) {
+        Map<String, Integer> charMap = new HashMap<>();
+        int oddCount = 0;
+
+        for (int i = 0; i < p_Str.length(); i++) {
+            String chr = ("" + p_Str.charAt(i)).toUpperCase();
+            if (charMap.get(chr) == null) {
+                charMap.put(chr, 1);
+                oddCount++;
+            } else {
+                int count = charMap.get(chr);
+                oddCount += (++count % 2 == 1) ? 1 : -1;
+                charMap.put(chr, count);
+            }
+        }
+
+        if (oddCount <= 1)
+            return true;
+        return false;
+    }
+
+    public static boolean isPalindrome(String p_Str) {
+        int n = p_Str.length() - 1;
+        for (int i = 0; i <= n / 2; i++) {
+            String chr1 = "" + p_Str.charAt(i);
+            String chr2 = "" + p_Str.charAt(n - i);
+            if (!chr1.equalsIgnoreCase(chr2))
+                return false;
+        }
+        return true;
+    }
+}
