2ae149a45d71825fb34a98c28adb4a54d7e0cdb0
Sandeep Ganjir <sandyganjir@gmail.com>
2020-07-19 13:50:06 +0530

    New Problems

---
+++ Java/Algorithmic/TreeNode.java
@@ -0,0 +1,14 @@
+/*
+ * Given an undirected acyclic graph, find the node which if 
+ * made root will result in a tree of minimum height.
+ * Input : a Graph
+ * Output : 5
+ * Expected Time Complexity: O(N).
+ * Expected Auxiliary Space: O(N).
+ *  @author Sandeep Ganjir
+ */
+
+import java.util.*;
+
+public class TreeNode {
+}
